a4f7d568f9c772b91d0e7a2604a1864fb85b8edb
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-12-13 14:18:44 +0500

    Added edit functionality to Smurf component

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import Form from './Form/Form';
 
 export default function Smurf({ smurf , deleteSmurf}) {
+    const [edit, setEdit] = useState(false);
   return (
     <div className="smurf">
       <p>
         {smurf.name} is {smurf.age} years old and is {smurf.height} tall.
       </p>
-      <button type = "button">Edit Smurf</button>
+      <button type = "button"onClick={event=>setEdit(!edit)}>Edit Smurf</button>
       <button type = "button" onClick = {event=>deleteSmurf(smurf.id)}>Delete Smurf</button>
+      {edit && <Form initialValues = {smurf} smurfID = {smurf.id}/>}
     </div>
   );
 }
